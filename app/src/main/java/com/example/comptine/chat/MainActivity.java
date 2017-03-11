0a97e6fca944b610051c1c7cf93a48a3bb2a5977
rovince <rovince.thu@gmail.com>
2017-03-11 10:26:57 +0800

    Chat_March_11th

---
+++ app/src/main/java/com/example/comptine/chat/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.comptine.chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<Msg> msgList = new ArrayList<>();
+    private EditText inputText;
+    private Button send;
+    private RecyclerView msgRecyclerView;
+    private MsgAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initMsgs();
+        inputText = (EditText) findViewById(R.id.input_text);
+        send = (Button) findViewById(R.id.send);
+        msgRecyclerView = (RecyclerView) findViewById(R.id.msg_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        msgRecyclerView.setLayoutManager(layoutManager);
+        adapter = new MsgAdapter(msgList);
+        msgRecyclerView.setAdapter(adapter);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = inputText.getText().toString();
+                if(!"".equals(content)){
+                    Msg msg = new Msg(content,Msg.TYPE_SENT);
+                    msgList.add(msg);
+                    adapter.notifyItemInserted(msgList.size() -1);
+                    msgRecyclerView.scrollToPosition(msgList.size() -1);
+                    inputText.setText("");
+                }
+            }
+        });
+    }
+    private void initMsgs(){
+        Msg msg1 = new Msg("Hello guy.",Msg.TYPE_RECEIVED);
+        Msg msg2 = new Msg("How are you recently?",Msg.TYPE_RECEIVED);
+        msgList.add(msg1);
+        msgList.add(msg2);
+    }
+}
